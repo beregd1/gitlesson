8d2f745e7dd7185688d7e2b6fe2e44da17bda07c
beregd1 <119509393+beregd1@users.noreply.github.com>
2022-12-10 15:12:27 +0200

    второе дз

---
+++ src/com/comp/lesson2/Homework2.java
@@ -0,0 +1,10 @@
+package com.comp.lesson2;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        task2();
+    }
+    private static void task2(){
+        System.out.println("Второе задание");
+    }
+}
